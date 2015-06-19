c14b6ba5238adca7dce027f030c38122062631ab
Charlie McClung <charlie@cmr1.com>
2015-06-19 17:21:08 -0600

    Better API response messaging

--- app/api.py
+++ app/api.py
@@ -14,11 +14,8 @@ response = {
 
 cookie_expiration = datetime.utcnow() + timedelta(days=config.get('cookie_life'))
 
-def bake_cookie(response, key, value):
-    response.set_cookie(key, value, expires=cookie_expiration)
-
-@app.route('/api/favorite/<class_name>/<int:id>')
-def favorite(class_name, id):
+@app.route('/api/favorite/<action>/<class_name>/<int:id>')
+def favorite(class_name, action, id):
     fav_cookie = request.cookies.get('favorites')
 
     if fav_cookie:
@@ -29,13 +26,22 @@ def favorite(class_name, id):
     try:
         obj = eval(class_name.capitalize()).get(id=id)
 
-        if class_name not in favorites:
-            favorites[class_name] = []
+        if action == 'remove':
+            if class_name in favorites and id in favorites[class_name]:
+                favorites[class_name].remove(id)
+                response['success'] = True
+            else:
+                response['errors'].append("Class: '%s' or %s id: %d not in favorites list!" % (class_name, class_name, id))
+        else:
+            if class_name not in favorites:
+                favorites[class_name] = []
 
-        if id not in favorites[class_name]:
-            favorites[class_name].append(id)
+            if id not in favorites[class_name]:
+                favorites[class_name].append(id)
+                response['success'] = True
+            else:
+                response['errors'].append("Already favorited %s %d" % (class_name, id))
 
-        response['success'] = True
         response['favorites'] = favorites
     except:
         response['errors'].append("Error initializing class: '%s' with id: %d" % (class_name, id))
@@ -44,6 +50,5 @@ def favorite(class_name, id):
 
     if response['success']:
         resp.set_cookie('favorites', json.dumps(favorites))
-        # bake_cookie(resp, 'favorites', json.dumps(favorites))
 
     return resp
