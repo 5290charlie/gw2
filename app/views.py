b8d470453f06ba04c640826a4f11b8effef8dd42
Charlie McClung <charlie@cmr1.com>
2015-06-19 17:33:23 -0600

    Fixing potential circular import issue

--- app/views.py
+++ app/views.py
@@ -72,3 +72,48 @@ def emblems(emblem_id=None):
         emblems = Emblem.select()
 
     return render_template('emblems.html', **locals())
+
+
+response = {
+    'success': False,
+    'errors': []
+}
+
+@app.route('/api/favorite/<action>/<class_name>/<int:id>')
+def favorite(class_name, action, id):
+    fav_cookie = request.cookies.get('favorites')
+
+    if fav_cookie:
+        favorites = json.loads(fav_cookie)
+    else:
+        favorites = {}
+
+    try:
+        obj = eval(class_name.capitalize()).get(id=id)
+
+        if action == 'remove':
+            if class_name in favorites and id in favorites[class_name]:
+                favorites[class_name].remove(id)
+                response['success'] = True
+            else:
+                response['errors'].append("Class: '%s' or %s id: %d not in favorites list!" % (class_name, class_name, id))
+        else:
+            if class_name not in favorites:
+                favorites[class_name] = []
+
+            if id not in favorites[class_name]:
+                favorites[class_name].append(id)
+                response['success'] = True
+            else:
+                response['errors'].append("Already favorited %s %d" % (class_name, id))
+
+        response['favorites'] = favorites
+    except:
+        response['errors'].append("Error initializing class: '%s' with id: %d" % (class_name, id))
+
+    resp = jsonify(response)
+
+    if response['success']:
+        resp.set_cookie('favorites', json.dumps(favorites))
+
+    return resp
